85db2c11f850eb4d42b50c377c679cda63318149
q <kenta@mit.edu>
2015-10-08 02:06:18 -0400

    empty log message

--- selfplay.pl
+++ selfplay.pl
@@ -3,15 +3,21 @@ use strict;
 use Chess::Rep;
 my$list='';
 my$timethink="depth 6";
+my$pos=Chess::Rep->new;
 for(@ARGV){
     die unless /^\S+$/;
+    print" $_";
     $list.=" $_";
+    die unless defined($pos->go_move($_));
 }
 for(;;){
     #print $list,"\n";
-    my$move=`perl bestmove.pl "$timethink" $list`;
-    print$move;
-    chomp$move;
-    last if $move eq '(none)';
-    $list .= " $move";
+    $_=`perl bestmove.pl "$timethink" $list`;
+    chomp;
+    print" $_";
+
+    last if $_ eq '(none)';
+    $list .= " $_";
+    die unless defined($pos->go_move($_));
 }
+print "\n";
