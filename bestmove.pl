0a3af3dbc5339d39fa1f75b269e5f30d8fbd24ac
q <kenta@mit.edu>
2015-10-08 02:12:46 -0400

    selfplay

--- bestmove.pl
+++ bestmove.pl
@@ -1,10 +1,7 @@
 #!perl -w
 use Expect;
 use strict;
-# stalemate, threefold repetition, fifty move rule
 $::command = "$ENV{HOME}/bin/stockfish";
-$::killswitch = "/tmp/killswitch";
-$::log = "/tmp/stockfish.log";
 &main;
 
 sub main {
@@ -23,18 +20,22 @@ sub main {
 sub start_engine {
     $::exp=Expect -> spawn($::command)
 	or die;
-    $::exp->log_stdout(0);
-    $::exp->log_file($::log);
+    if(defined($ENV{chesslog})){
+        $::exp->log_stdout(0);
+        $::exp->log_file($ENV{chesslog});
+    }
     #getting these line endings wrong results in very confusing behavior
     #the line endings also depend on the -l switch to perl
-    $::exp->expect(15, ("Kiiski\r\n")) or die;
+    $::exp->expect(15, ("\r\n")) or die;
     $::exp->send("uci\r");
     $::exp->expect(5,("uciok\r\n")) or die;
     $::exp->send("setoption name Threads value 4\r");
-    $::exp->send("setoption name Hash value 900\r");
-    $::exp->send("setoption name Clear Hash\r");
+    #$::exp->send("setoption name Hash value 900\r");
+    #$::exp->send("setoption name Clear Hash\r");
 #    $::exp->send("setoption name MultiPV value 400\r");
     $::exp->send("isready\r");
+    $::exp->expect(undef,("readyok\r\n")) or die;
+    $::exp->send("ucinewgame\risready\r");
     $::exp->expect(undef,("readyok\r\n")) or die;
 }
 
@@ -47,10 +48,6 @@ sub engine {
     $::exp->send("position startpos moves$movelist1\rgo $timethink\r");
     my $successfully_matching_string;
     for(;;){
-	if (-e $::killswitch) {
-	    unlink $::killswitch;
-	    die "killswitch internal";
-	}
 	#($matched_pattern_position, $error, $successfully_matching_string, $before_match,       $after_match);
         #the ordering of the expect clauses matter.
         # sometimes the event loop sees the sum of two lines at once.
--- selfplay.pl
+++ selfplay.pl
@@ -1,23 +1,57 @@
 #! perl -w
 use strict;
 use Chess::Rep;
+die unless defined $ENV{chesslog};
+die if -e $ENV{chesslog};
 my$list='';
-my$timethink="depth 6";
+my$nodes_per_minute=36_000_000*10.;
+my$nodes_per_hour=$nodes_per_minute*60;
+my$total_hours=0.5/60;
+my$total_nodes=$total_hours*$nodes_per_hour;
+my$total_moves=80;
+my$nodes=int($total_nodes/$total_moves);
+my$timethink="nodes $nodes";
+#6.25s per million x 10 moves
 my$pos=Chess::Rep->new;
 for(@ARGV){
     die unless /^\S+$/;
-    print" $_";
+    print"$_ ";
     $list.=" $_";
     die unless defined($pos->go_move($_));
 }
-for(;;){
+print '|';
+#not counting repetitions in the opening
+my %repetitions;
+for(my$i=0;;++$i){
     #print $list,"\n";
     $_=`perl bestmove.pl "$timethink" $list`;
     chomp;
     print" $_";
 
-    last if $_ eq '(none)';
+    if($_ eq '(none)'){
+        print "\n";
+        print "mate\n" if $pos->status->{mate};
+        print "stalemate\n" if $pos->status->{stalemate};
+        last;
+    }
     $list .= " $_";
     die unless defined($pos->go_move($_));
+    my$fen=$pos->get_fen;
+    #print "$fen\n";
+    #print $pos->dump_pos,"\n";
+    my($rle,$color,$castle,$enpassant,$fifty,$movecount)=split ' ',$fen;
+    die unless $color eq 'w' or $color='b';
+    die unless $fifty =~ /^\d+$/;
+    die unless $movecount =~ /^\d+$/;
+    if($fifty>=50){
+        print "\nfifty move rule\n";
+        last;
+    }
+    my$state="$rle $color $castle $enpassant"; #tricky question as to whether castling and enpassant matter in three-fold repetition;
+    $repetitions{$state}++;
+    #print "repetitions $repetitions{$state}\n";
+    if($repetitions{$state}>=3){
+        print"\nthree-fold repetition\n";
+        last;
+    }
 }
-print "\n";
