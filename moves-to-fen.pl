9a9872554ef91befd82658c7a5f773a8bfb2ae18
Ken T Takusagawa <kenta@mit.edu>
2015-10-11 05:13:54 -0400

    Now more of a swiss army knife of providing different facts about a SAN move sequence.

--- moves-to-fen.pl
+++ moves-to-fen.pl
@@ -1,8 +1,27 @@
-#!perl -wl
+#!perl -w
 use Chess::Rep;
-my$pos=Chess::Rep->new;
+use Getopt::Long;
+GetOptions('fen'=>\$fen,
+           'list'=>\$dolist,
+           'moves'=>\$moves,
+    );
+$list='';
+$pos=Chess::Rep->new;
 for(@ARGV){
     die unless /^\S+$/;
-    die unless defined($pos->go_move($_));
+    my$details;
+    die unless defined($details=$pos->go_move($_));
+    my$construct=lc($details->{from}.$details->{to});
+    $list.=" $construct";
 }
-print$pos->get_fen;
+print "fen ",$pos->get_fen,"\n" if $fen;
+print "list",$list,"\n" if $dolist;
+if($moves){
+    print"moves";
+    $status=$pos->status;
+    $moves=$status->{moves};
+    for(@$moves){
+        print " ",lc(Chess::Rep::get_field_id($$_{from})),lc(Chess::Rep::get_field_id($$_{to}));
+    }
+    print"\n";
+}
