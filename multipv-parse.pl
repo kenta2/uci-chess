0305c68979b43723db0f155671455fb1a6fc097f
Ken T Takusagawa <kenta@mit.edu>
2017-02-17 01:48:38 -0500

    redo the logic entirely

--- multipv-parse.pl
+++ multipv-parse.pl
@@ -1,46 +1,54 @@
 #! perl -lw
 # standalone parser of a log of a multipv run.
 # not used in any pipeline.
-
+$found=0;
 while(<>){
-    last if /^info depth/;
+    if(/^info depth/){
+        $found=1;
+        last
+    }
 }
+die unless$found;
+#this discards first item, but whatever.
+$largest=0;
+$deepest=0;
+$maxnodes=0;
 while(<>){
     #chomp;
     s/\r?\n//;
-    if(/^info depth (?<depth>\d+) seldepth \d+ multipv (?<multipv>\d+) score (?<score>.+) nodes (?<nodes>\d+) nps \d+ (?:hashfull \d+ )?tbhits \d+ time \d+ pv (?<pv>\S+)/){
-        $running{$+{multipv}}="$+{pv} depth $+{depth} score $+{score}";
-        $depth=$+{depth};
-        $nodes=$+{nodes}
-        #print "found pv $+{depth} $+{pv} ",scalar(%running);
+    #print "a $_";
+    if(($depth,$multipv,$score,$nodes,$pv)=/^info depth (\d+) seldepth \d+ multipv (\d+) score (.+) nodes (\d+) nps \d+ (?:hashfull \d+ )?tbhits \d+ time \d+ pv (\S+)/){
+        if($multipv>$largest){
+            $largest=$multipv;
+        }
+        if($depth>$deepest){
+            $deepest=$depth;
+        }
+        $seen{"$depth $multipv"}="$pv $score";
+        die if $nodes<$maxnodes;
+        $maxnodes=$nodes;
     }
-    elsif (/^info depth (?<depth>\d+) currmove .+ currmovenumber (?<num>\d+)$/){
-        $num=$+{num};
-        $new=$+{depth};
-        if($num==1 and %running){
-            #sometimes the 1 appears twice
-            die unless $depth==($new-1);
-            $cdepth=$depth;
-            %completed=%running;
-            undef%running;
-            for(sort {$a<=>$b} keys %completed){
-                print"$_ $completed{$_}";
-                die unless ($indepth) = ($completed{$_} =~ /depth (\d+)/);
-                die unless $indepth==($new-1);
-            }
-            print "starting depth $new nodes $nodes";
+    elsif (($depth,$multipv)=/^info depth (\d+) currmove .+ currmovenumber (\d+)$/){
+        if($multipv>$largest){
+            $largest=$1;
+        }
+        if($depth>$deepest){
+            $deepest=$depth;
         }
     }elsif(/^info nodes (\d+) time \d+$/){
-        $nodes=$1
+        $nodes=$1;
+        $maxnodes=$nodes;;
     }elsif(/^bestmove/){
         print "loop exit via $_";
         last;
     }else{
-        print;
+        die;
     }
 }
-#for(sort {$a<=>$b} keys %completed){    print"$_ $completed{$_}";}
-die unless defined $completed{1};
-print "best multipv $completed{1}";
-print "completed depth $cdepth";
-print "final nodes $nodes";
+unless(defined$seen{"$deepest $largest"}){
+    $deepest--;
+    print "incomplete final depth";
+}
+die unless defined $seen{"$deepest $largest"};
+die unless defined $seen{"$deepest 1"};
+print "bestmove ",$seen{"$deepest 1"}," completed_depth $deepest maxnodes $maxnodes nummoves $largest";
